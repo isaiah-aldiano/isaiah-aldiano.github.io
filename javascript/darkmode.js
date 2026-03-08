55e057b5fcdf5b140653744aaae4ccdf29cc0ef3
Isaiah Aldiano <iealdiano@gmail.com>
2026-03-08 10:36:53 -0500

    Updated css and javascript functions. Added supporting documents for projects

--- javascript/darkmode.js
+++ javascript/darkmode.js
@@ -1,26 +1,32 @@
 function toggleTheme() {
   const isDark = document.getElementById("themeToggle").checked;
+
+  setTheme(isDark);
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+}
+
+function setTheme(dark) {
   const body = document.body;
   const html = document.documentElement;
-
-  if (isDark) {
+  const githubIcon = document.getElementById("github");
+  const linkedinIcon = document.getElementById("linkedin");
+  if (dark) {
     body.classList.remove("theme-light");
     body.classList.add("theme-dark");
     html.setAttribute("data-bs-theme", "dark");
-    localStorage.setItem("theme", "dark");
+    githubIcon.src = githubIcon.dataset.dark;
+    linkedinIcon.src = linkedinIcon.dataset.dark;
   } else {
     body.classList.remove("theme-dark");
     body.classList.add("theme-light");
     html.setAttribute("data-bs-theme", "light");
-    localStorage.setItem("theme", "light");
+    githubIcon.src = githubIcon.dataset.light;
+    linkedinIcon.src = linkedinIcon.dataset.light;
   }
 }
 
 // Load on page start
 window.onload = () => {
-  const saved = localStorage.getItem("theme");
-  if (saved === "dark") {
-    document.getElementById("themeToggle").checked = true;
-    toggleTheme();
-  }
+  const contDark = localStorage.getItem("theme") === "dark" ? true : false;
+  setTheme(contDark);
 };
